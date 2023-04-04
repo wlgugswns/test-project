aee7b4c78cbfc5fc33b1fa5bde4864223e8da9e5
wlgugswns <jhj010773@gmail.com>
2023-04-04 10:41:46 +0900

    wlgugswns, This is the beginning of the Snake Bite Game

---
+++ Snake_Bite/Snake_Bite.cpp
@@ -0,0 +1,132 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+#include <Windows.h>
+
+int print_title_screen()
+{
+	printf("********************************************\n");
+	printf("*                                          *\n");
+	printf("*                                          *\n");
+	printf("*               지렁이 게임                *\n");
+	printf("*              (Snake Bite)                *\n");
+	printf("*                                          *\n");
+	printf("*   1. 게임 시작                           *\n");
+	printf("*   2. 게임 설명                           *\n");
+	printf("*   3. 게임 랭킹 보기                      *\n");
+	printf("*   4. 게임 종료 (esc)                     *\n");
+	printf("********************************************\n");
+
+
+	return 0;
+}
+
+int print_introduction_screen()
+{
+	printf("********************************************\n");
+	printf("*          게임 설명 화면입니다.           *\n");
+	printf("********************************************\n");
+	printf("*    타이틀 화면으로 돌아갈까요? (Y/N)     *\n");
+
+	return 0;
+}
+
+void gotoxy(int x, int y)
+{
+	COORD Pos = { x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
+}
+
+int print_stage_screen(int widht, int height)
+{
+	gotoxy(100, 100);
+	printf(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
+	printf("^                                          ^\n");
+	printf("^                                          ^\n");
+	printf("^                                          ^\n");
+	printf("^                                          ^\n");
+	printf("^                                          ^\n");
+	printf("^                                          ^\n");
+	printf("^                                          ^\n");
+	printf("^                                          ^\n");
+	printf("^                                          ^\n");
+	printf("^                                          ^\n");
+	printf("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n");
+	printf("********************************************\n");
+	printf("*    타이틀 화면으로 돌아갈까요? (Y/N)     *\n");
+	return 0;
+}
+
+int main()
+{
+	int game_state = 0;
+	int is_game_running = 1;
+	int refresh = 0;
+	while (is_game_running)
+	{
+		char key_input = 0;
+		switch (game_state)
+		{
+		case 0:
+			if (refresh == 0)
+			{
+				print_title_screen();
+				refresh = 10;
+			}
+			key_input = 0;
+			key_input = _getch();
+
+			if (key_input == '1')
+			{
+				game_state = 1;
+				refresh = 1;
+			}
+			else if (key_input == '2')
+			{
+				game_state = 2;
+				refresh = 2;
+			}
+			else if (key_input == '4' || key_input == 27)
+			{
+				is_game_running = 0;
+			}
+			break;
+
+		case 1:
+			if (refresh == 1)
+			{
+				print_stage_screen(10, 10);
+				refresh = 10;
+			}
+			key_input = 0;
+			key_input = _getch();
+
+			if (key_input == 'y')
+			{
+				game_state = 0;
+				refresh = 0;
+			}
+			
+			break;
+
+		case 2:
+			if (refresh == 2)
+			{
+				print_introduction_screen();
+				refresh = 10;
+			}
+			key_input = 0;
+			key_input = _getch();
+
+			if (key_input == 'y')
+			{
+				game_state = 0;
+				refresh = 0;
+			}
+			break;
+		default:
+			break;
+		}
+	}
+	return 0;
+}
